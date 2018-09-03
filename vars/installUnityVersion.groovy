f6becca2743f436672ab29e74bd873243888f010
Benedikt Reiser <benedikt.reiser@gmail.com>
2018-09-03 15:41:56 +0200

    joined all global variables in one file

--- vars/installUnityVersion.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(String unityVersion) {
-  echo "Trying to install Unity Version ${unityVersion}."
-  powershell "gem install u3d"
-  powershell "u3d install ${unityVersion}"
-}
--- vars/unity.groovy
+++ vars/unity.groovy
@@ -1,8 +1,13 @@
 #!/usr/bin/env groovy
 
-def call(String unityVersion, String projectPath, String unityEmail, String unityPassword, String unitySerial) {
-  echo "Activating Unity License for ${email}"
-  // Unity is stupid so we need to open it once without batchmode before actually trying to start the license activation process
+def install(unityVersion) {
+  echo "Trying to install Unity Version ${unityVersion}."
+  powershell "gem install u3d"
+  powershell "u3d install ${unityVersion}"
+}
+
+def createProject(unityVersion, projectPath, unityEmail, unityPassword, unitySerial) {
+  // Unity has some issues with batchmode so we need to open it once without batchmode before actually trying to start the license activation process
   // this might be fixed in a newer version, but for now we're stuck. see http://answers.unity.com/answers/1171215/view.html
   echo "Starting Unity once (non-headless) for license activation"
   powershell """
@@ -19,6 +24,11 @@ def call(String unityVersion, String projectPath, String unityEmail, String unit
       Taskkill /IM Unity.exe /F
   }
   """
-  echo "Activating Unity license & Creating new Unity Project"
+  echo "Activating Unity license for ${unityEmail} & Creating new Unity Project"
   powershell "u3d run -u ${unityVersion} -- -nographics -quit -batchmode -logFile editor.log -createProject ${projectPath} -serial \"${unitySerial}\" -username \"${unityEmail}\" -password \"${unityPassword}\""
+}
+
+def returnSerial(unityVersion) {
+  echo "Returning Unity Serial"
+  powershell "u3d run -u ${unityVersion} -- -nographics -quit -batchmode -logFile editor.log -returnlicense"
 }
